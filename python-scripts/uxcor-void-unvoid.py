529dc3f4f253e83e2ef32e52bcd1e505137f27f1
aldenparoni <aparoni@hawaii.edu>
2023-07-28 11:10:28 -1000

    Update uxcor-void-unvoid.py

--- python-scripts/uxcor-void-unvoid.py
+++ python-scripts/uxcor-void-unvoid.py
@@ -2,14 +2,12 @@ from unifier_requests.bplib import uxcor
 
 env = 'prod'
 shells = [
-    'DB200', 'DB320', 'DB450', 'DBB171', 'DBB271', 'DBB371', 'DBB385',
-    'DBB505', 'DBB511', 'DBB511.1', 'DBB511.2', 'DBB525', 'DBB602', 'DBB701', 'DBB701.1',
-    'DBB921', 'DBOM920', 'FD140', 'FD240', 'FD340', 'FD430', 'FD440', 'FD530',
-    'FD550', 'FD700', 'FD701', 'HART201', 'HART202', 'MI900', 'MI930', 'MI940', 'MM920', 
-    'MM595', 'MM596', 'MM900', 'MM901', 'MM902', 'MM905', 'MM910', 'MM913', 'MM915', 'MM920',
-    'MM921', 'MM922', 'MM925', 'MM930', 'MM935', 'MM936', 'MM937', 'MM940', 'MM941', 'MM945',
-    'MM950', 'MM951', 'MM953', 'MM960', 'MM962', 'MM964', 'MM970', 'MM975', 'MM980', 'MM981',
-    'MM982', 'MM983', 'MM985', 'MM986', 'MM990', 'MM991', 'PA102', 'UTIL'
+    'DB200', 'DB320', 'DB450', 'DBB171', 'DBB271', 'DBB371', 'DBB385', 'DBB505', 'DBB511', 'DBB511.1',
+    'DBB511.2', 'DBB525', 'DBB602', 'DBB701', 'DBB701.1', 'DBB921', 'DBOM920', 'UTIL', 'FD140', 'FD240',
+    'FD340', 'FD430', 'FD530', 'FD550', 'FD700', 'FD701', 'MI900', 'MI930', 'MM940', 'MM595', 'MM596',
+    'MM900', 'MM901', 'MM902', 'MM905', 'MM910', 'MM913', 'MM915', 'MM920', 'MM921', 'MM922', 'MM925',
+    'MM930', 'MM935', 'MM936', 'MM937', 'MM940', 'MM941', 'MM945', 'MM950', 'MM951', 'MM953', 'MM960',
+    'MM962', 'MM964', 'MM970', 'MM975', 'MM981', 'MM982', 'MM983', 'MM985', 'MM986', 'MM990', 'MM991'
 ]
 folder = r'C:\Users\alden.paroni\uxcor-void-unvoid'
 file_void = '-void.csv'
